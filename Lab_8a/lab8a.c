7692f4a4ccaa3e626ae47521a9d81d6e9e2d7d0e
vishsiri <s.vish555@gmail.com>
2022-10-12 07:40:35 -0400

    new: lab_8a

--- Lab_8a/lab8a.c
+++ Lab_8a/lab8a.c
@@ -1,2 +1,31 @@
 // 64050229
 // Vish Siriwatana
+// Q1
+// 20! = 2432902008176640000
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+
+int main(void) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        printf("child created\n");
+        while(1) {
+            printf("Please kill me\n");
+            sleep(1);
+        }
+        printf("This line not show sir :)");
+        exit(0);
+    } else {
+        sleep(1);
+        for (int i = 5; i > 0; i--) {
+            printf("parentPS will kill childPS in %d\n", i);
+            sleep(1);
+        }
+        kill(pid, SIGTERM);
+    }
+}
